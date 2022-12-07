53d0b62443273effef5ee60e05e4b3c16de9e65c
ildar789 <ildar-sh2002@yandex.ru>
2022-12-07 05:01:13 +0300

    commit

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Нанесение урона."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5+randint(3, 5)}')
     if char_class == 'mage':
         return (f'{char_name} нанёс урон противнику равный {5+randint(5, 10)}')
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный {5+randint(-3,-1)}')
+    return ''
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """"""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+    return ''
 
 
 def special(char_name: str, char_class: str) -> str:
@@ -28,6 +32,7 @@ def special(char_name: str, char_class: str) -> str:
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return ''
 
 
 def start_training(char_name: str, char_class: str) -> str:
