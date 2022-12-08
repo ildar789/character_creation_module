a7c937096a03018e2a0e4e8641c03026e011acf6
ildar789 <ildar-sh2002@yandex.ru>
2022-12-08 03:45:15 +0300

    commit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 DEFAULT_ATTACK = 5
-DEFAULT_DEFENCE = 10 
-DEFAULT_STAMINA = 80 
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
+
 
 class Character:
     # Новая константа.
@@ -32,6 +33,7 @@ class Character:
     def __str__(self):
         return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
 
+
 class Warrior(Character):
     BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
                              'Сильный, выносливый и отважный')
@@ -39,6 +41,7 @@ class Warrior(Character):
     RANGE_VALUE_DEFENCE = (5, 10)
     SPECIAL_BUFF = DEFAULT_STAMINA + 25
     SPECIAL_SKILL = 'Выносливость'
+
 
 class Mage(Character):
     BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
@@ -48,13 +51,14 @@ class Mage(Character):
     SPECIAL_BUFF = DEFAULT_ATTACK + 40
     SPECIAL_SKILL = 'Атака'
 
+
 class Healer(Character):
     BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
                              'Черпает силы из природы, веры и духов')
     RANGE_VALUE_ATTACK = (-3, -1)
     RANGE_VALUE_DEFENCE = (2, 5)
     SPECIAL_BUFF = DEFAULT_DEFENCE + 30
-    SPECIAL_SKILL = 'Защита' 
+    SPECIAL_SKILL = 'Защита'
 
 
 def start_training(character):
@@ -63,9 +67,9 @@ def start_training(character):
     Возвращает сообщения о результатах цикла тренировки персонажа.
     """
     # Замените конструкцию условных операторов на словарь.
-    commands = {'attack': attack,
-                'defence': defence,
-                'special': spaecial}
+    commands = {'attack': character.attack(),
+                'defence': character.defence(),
+                'special': character.spaecial()}
     cmd = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
@@ -74,7 +78,7 @@ def start_training(character):
         # В функции print() будет вызываться метод класса,
         # который соответствует введённой команде.
         if cmd in commands:
-            print(commands[cmd]())
+            print(commands[cmd])
     return 'Тренировка окончена.'
 
 
@@ -83,31 +87,29 @@ def choice_char_class(char_name: str) -> Character:
     Возвращает строку с выбранным
     классом персонажа.
     """
-    # Добавили словарь, в котором соотносится ввод пользователя и класс персонажа.
+    # Cловарь, в котором соотносится ввод пользователя и класс персонажа.
     game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
-    
-    approve_choice: str  = None
-    
+    approve_choice = None
     while approve_choice != 'y':
         selected_class = input('Введи название персонажа, '
-                           'за которого хочешь играть: Воитель — warrior, '
-                           'Маг — mage, Лекарь — healer: ')
+                               'за которого хочешь играть: Воитель — warrior, '
+                               'Маг — mage, Лекарь — healer: ')
         char_class: Character = game_classes[selected_class](char_name)
         # Вывели в терминал описание персонажа.
         print(char_class)
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
-    return char_class 
+    return char_class
 
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
-            'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     warrior = Warrior('Кодослав')
